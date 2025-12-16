e17fe3f77629c5e97d3c95eb40b154e4bec414bc
kharanh112 <maikhaanh11205@gmail.com>
2025-12-16 15:43:27 +0700

    Squashed 'frontend/' content from commit 798745f5
    git-subtree-dir: frontend
    git-subtree-split: 798745f58c2309091aa3069d88ef9cfcf4b7ab49

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+  },
+  server: {
+    host: true,
+    port: 5173,
+    watch: {
+      usePolling: true,
+    },
+    proxy: {
+      '/api': {
+        target: 'https://backend-smartcard-production.up.railway.app',
+        changeOrigin: true,
+      },
+    },
+  },
+});
